6076b9c129de33c3d3b6ba4539c4c512a2c32694
Tim Holy <tim.holy@gmail.com>
2021-07-30 14:48:12 -0500

    Comment out precompile

--- src/GridLayoutBase.jl
+++ src/GridLayoutBase.jl
@@ -39,9 +39,9 @@ export protrusionsobservable, suggestedbboxobservable, reportedsizeobservable, a
 export ncols, nrows
 export contents, content
 
-if Base.VERSION >= v"1.4.2"
-    include("precompile.jl")
-    _precompile_()
-end
+# if Base.VERSION >= v"1.4.2"
+#     include("precompile.jl")
+#     _precompile_()
+# end
 
 end
