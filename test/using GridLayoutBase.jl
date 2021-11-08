be27c98c8378890d694749a66ee634269ed4343e
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2021-11-08 22:13:25 +0100

    factor out _compute_content_bbox

---
+++ test/using GridLayoutBase.jl
@@ -0,0 +1,17 @@
+using GridLayoutBase
+using CairoMakie
+
+##
+f = Figure()
+g = f[1, 1] = GridLayout()
+Axis(g[1, 1])
+Colorbar(g[1, 2])
+colsize!(g, 1, Aspect(1, 1))
+Axis(f[2, 1])
+f
+
+
+bbox = BBox(0, 1000, 0, 1000)
+layout = GridLayout(bbox = bbox, alignmode = Mixed(left = 0, top = 100))
+dr = layout[1, 1] = DebugRect()
+@time GridLayoutBase.compute_rowcols(layout, bbox)
